dcef2507f91d670cbf8f27d926e14c84465d4044
sgaravat <massimo.sgaravatto@pd.infn.it>
2020-11-30 15:48:36 +0100

    Applicate le istruzioni di Gravino per bacula su centos8

--- compute_train/manifests/bacula.pp
+++ compute_train/manifests/bacula.pp
@@ -2,17 +2,30 @@ class compute_train::bacula {
 
 include compute_train::params
 
-   $baculapackages = [ "bacula-client" ]
+   $baculapackages = [ "bacula-client" , "bacula-libs", "bacula-common" ]
   
-     package { $baculapackages: ensure => "installed" }
+   package { $baculapackages: ensure => "purged" }
+
+   package {"bareos-common":
+             provider => "rpm",
+             ensure   => installed,
+             source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-common-19.2.7-2.el8.x86_64.rpm",
+           }  
+ 
+   package {"bareos-filedaemon":
+             provider => "rpm",
+             ensure   => installed,
+             source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
+             require => Package["bareos-common"],
+           }  
   
     
-     service { "bacula-fd":
+     service { "bareos-fd":
         ensure => running,
         enable => true,
         hasstatus => true,
         hasrestart => true,
-        require => Package["bacula-client"],
+        require => Package["bareos-filedaemon"],
       }
 
 
