d608c29d864399b3248bcc292994ff66c7d6fe83
sgaravat <massimo.sgaravatto@pd.infn.it>
2020-12-09 11:29:38 +0100

    Sistemato bacula per centos7

--- compute_train/manifests/bacula.pp
+++ compute_train/manifests/bacula.pp
@@ -2,31 +2,45 @@ class compute_train::bacula {
 
 include compute_train::params
 
-   $baculapackages = [ "bacula-client" , "bacula-libs", "bacula-common" ]
-  
-   package { $baculapackages: ensure => "purged" }
+$baculapackages = [ "bacula-client" , "bacula-libs", "bacula-common" ]
 
-   package {"bareos-common":
-             provider => "rpm",
-             ensure   => installed,
-             source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-common-19.2.7-2.el8.x86_64.rpm",
-           }  
+
+case $operatingsystemrelease {
+    /^7.*/: {
+             package { $baculapackages: ensure => "installed" }
+             service { "bacula-fd":
+                       ensure => running,
+                       enable => true,
+                       hasstatus => true,
+                       hasrestart => true,
+                       require => Package["bacula-client"],
+                     }
+            }
+    /^8.*/: {
+  
+              package { $baculapackages: ensure => "purged" }
+
+              package {"bareos-common":
+                 provider => "rpm",
+                 ensure   => installed,
+                 source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-common-19.2.7-2.el8.x86_64.rpm",
+                      }  
  
-   package {"bareos-filedaemon":
-             provider => "rpm",
-             ensure   => installed,
-             source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
-             require => Package["bareos-common"],
-           }  
+              package {"bareos-filedaemon":
+                 provider => "rpm",
+                 ensure   => installed,
+                 source => "http://download.bareos.org/bareos/release/latest/CentOS_8/x86_64/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
+                 require => Package["bareos-common"],
+                      }  
   
-    
-     service { "bareos-fd":
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        hasrestart => true,
-        require => Package["bareos-filedaemon"],
-      }
+              service { "bareos-fd":
+                 ensure => running,
+                 enable => true,
+                 hasstatus => true,
+                 hasrestart => true,
+                 require => Package["bareos-filedaemon"],
+                      }
 
-
+            }
+}
 }
